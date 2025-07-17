37f987e2a09a73d061800633a8f14d13f67d6c93
meidhika <meidhikanawa@gmail.com>
2025-07-17 21:10:45 +0700

    Sidebar Menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import { DarkModeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button>Hello</Button>
-      <DarkModeToggle />
-      <div></div>
+    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+      <h1 className="text-4xl font-semibold ">Welcome Meidhika Nawa Sapta</h1>
+      <Link href="/admin">
+        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
