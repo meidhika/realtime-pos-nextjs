a7660e15e46f43064cdab6e2269d1453b1c2438c
meidhika <meidhikanawa@gmail.com>
2025-07-27 13:19:53 +0700

    Feat: Detail Order

---
+++ src/app/(dashboard)/order/[id]/page.tsx
@@ -0,0 +1,13 @@
+import DetailOrder from "./_components/detail-order";
+
+export const metadata = {
+  title: "NS Store | Order Management",
+};
+export default async function DetailOrderPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return <DetailOrder id={id} />;
+}
