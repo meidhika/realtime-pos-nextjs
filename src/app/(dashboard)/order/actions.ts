d75c4b9597b865547443489e48d07f182423a98e
meidhika <meidhikanawa@gmail.com>
2025-07-27 12:48:59 +0700

    Feat:Reservation

--- src/app/(dashboard)/order/actions.ts
+++ src/app/(dashboard)/order/actions.ts
@@ -1,13 +1,11 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
+import { FormState } from "@/types/general";
 import { OrderFormState } from "@/types/order";
 import { orderFormSchema } from "@/validations/order-validation";
 
-export async function createOrder(
-  prevState: OrderFormState,
-  formData: FormData
-) {
+export async function createOrder(prevState: FormState, formData: FormData) {
   const validatedFields = orderFormSchema.safeParse({
     customer_name: formData.get("customer_name"),
     table_id: formData.get("table_id"),
@@ -44,6 +42,48 @@ export async function createOrder(
             : "unavailable",
       })
       .eq("id", validatedFields.data.table_id),
+  ]);
+
+  const orderError = orderResult.error;
+  const tableError = tableResult.error;
+
+  if (orderError || tableError) {
+    return {
+      status: "error",
+      errors: {
+        ...prevState.errors,
+        _form: [
+          ...(orderError ? [orderError.message] : []),
+          ...(tableError ? [tableError.message] : []),
+        ],
+      },
+    };
+  }
+
+  return {
+    status: "success",
+  };
+}
+
+export async function updateReservation(
+  prevState: OrderFormState,
+  formData: FormData
+) {
+  const supabase = await createClient();
+  const [orderResult, tableResult] = await Promise.all([
+    supabase
+      .from("orders")
+      .update({
+        status: formData.get("status"),
+      })
+      .eq("id", formData.get("id")),
+    supabase
+      .from("tables")
+      .update({
+        status:
+          formData.get("status") === "process" ? "unavailable" : "available",
+      })
+      .eq("id", formData.get("table_id")),
   ]);
 
   const orderError = orderResult.error;
